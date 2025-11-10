9637648f84ce151596b02d1656a08b62d0c2aa28
shubham56-h <shubu56143@email.com>
2025-11-10 21:59:15 +0530

    deploy try 2

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Run database migrations
-python -m flask db upgrade
-
-# Start the application with gunicorn
-# Railway automatically sets PORT environment variable
-gunicorn run:app --bind 0.0.0.0:${PORT:-8000}
